e7448db6bce64a69608308341b30eb45c6695e71
laisvitoria10012009-stack <laisvitoria10012009@gmail.com>
2026-09-22 14:06:48 -0300

    feat: create Explorar page with initial state and data structure

---
+++ frontend/src/pages/Explorar.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import "./Explorar.css";
+
+function Explorar() {
+  const [filtroSelecionado, setFiltroSelecionado] = useState("Todos");
+
+  const objetos = [
+    {
+      nome: "Kepler-452b",
+      tipo: "Exoplaneta",
+      distancia: "1.400 anos-luz",
+      categoria: "Exoplaneta",
+      imagem:
+        "https://upload.wikimedia.org/wikipedia/commons/e/ed/Kepler-452b_artist_concept.jpg?utm_source=pt.wikipedia.org&utm_campaign=index&utm_content=original",
+    },
+    {
+      nome: "HD 189733b",
+      tipo: "Exoplaneta",
+      distancia: "63 anos-luz",
+      categoria: "Gigante Gasoso",
+      imagem:
+        "https://upload.wikimedia.org/wikipedia/commons/8/80/Artist%E2%80%99s_impression_of_the_deep_blue_planet_HD_189733b.jpg?utm_source=pt.wikipedia.org&utm_campaign=index&utm_content=original",
+    },
+    {
+      nome: "Proxima Centauri b",
+      tipo: "Exoplaneta",
+      distancia: "4,24 anos-luz",
+      categoria: "Planeta Terrestre",
+      imagem:
+        "https://s2.glbimg.com/AwZbHRpIVjtalqmwR9_TksthVIw=/e.glbimg.com/og/ed/f/original/2016/09/12/proxima-b-habitable-zone-exoplanet-illustration-2x1-phl-upl.png",
+    },
+    {
+      nome: "TRAPPIST-1e",
+      tipo: "Exoplaneta",
+      distancia: "39 anos-luz",
+      categoria: "Planeta Terrestre",
+      imagem:
+        "https://upload.wikimedia.org/wikipedia/commons/5/5f/TRAPPIST-1e_Artist%27s_Impression.png?utm_source=pt.wikipedia.org&utm_campaign=index&utm_content=original",
+    },
+    {
+      nome: "Nebulosa do Caranguejo",
+      tipo: "Nebulosa",
+      distancia: "6.500 anos-luz",
+      categoria: "Nebulosa",
+      imagem:
+        "https://thumb.wikimedia.org/wikipedia/commons/thumb/0/00/Crab_Nebula.jpg/960px-Crab_Nebula.jpg?utm_source=pt.wikipedia.org&utm_campaign=parser&utm_content=thumbnail",
+    },
+    {
+      nome: "Galáxia de Andrômeda",
+      tipo: "Galáxia",
+      distancia: "2,537 milhões de anos-luz",
+      categoria: "Galáxia",
+      imagem:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaGT9ZXtsg9b8XoKD1WfNGx5xxXJHBXBQHzs1ewyZTNw&s=10",
+    },
+    {
+      nome: "55 Cancri e",
+      tipo: "Exoplaneta",
+      distancia: "41 anos-luz",
+      categoria: "Super-Terra",
+      imagem:
+        "https://live-production.wcms.abc-cdn.net.au/517b569c883c94e18a8100c9136c337b?impolicy=wcms_crop_resize&cropH=438&cropW=659&xPos=7&yPos=0&width=862&height=575",
+    },
+    {
+      nome: "GJ 1214b",
+      tipo: "Exoplaneta",
+      distancia: "48 anos-luz",
+      categoria: "Super-Terra",
+      imagem:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd1U22HINDUiy59EqSlo6CrKMLVBkANjmdaT0OB1dtcnSF777z-KovXg0&s=10",
+    },
+  ];
